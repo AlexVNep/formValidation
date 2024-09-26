30a10abca8e9c2187b0d0b907373ddee08d59173
Alex <alexvnep@gmail.com>
2024-09-26 13:42:36 +0200

    Add event listener for country input and error messages based on requirements

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const form = document.querySelector("form");
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.error");
 
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span.error");
+
 email.addEventListener("input", (event) => {
   if (email.validity.valid) {
     emailError.textContent = "";
     emailError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+country.addEventListener("input", (event) => {
+  if (country.validity.valid) {
+    countryError.textContent = "";
+    countryError.className = "error";
   } else {
     showError();
   }
@@ -26,6 +38,21 @@ function showError() {
     emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
   }
 
+  if (country.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    countryError.textContent = "You need to enter a country name.";
+  } else if (country.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    countryError.textContent = "Entered value needs to be text.";
+  } else if (country.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    countryError.textContent = `Country should be at least ${country.minLength} characters and less than ${country.maxLength}; you entered ${country.value.length}.`;
+  }
+
   // Set the styling appropriately
   emailError.className = "error active";
+  countryError.className = "error active";
 }
